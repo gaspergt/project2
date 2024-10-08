3cf20c875edde4f96d6c3abff115d83731c5a882
luisixc <luislima.ixc@gmail.com>
2024-10-08 11:21:12 -0600

    clean main

--- src/main/java/com/notes/notesapp/repositories/NotaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.notes.notesapp.repositories;
-
-import com.notes.notesapp.models.Nota;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotaRepository extends JpaRepository<Nota, Long> {
-}
